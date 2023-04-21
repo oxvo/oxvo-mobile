75edf4c96cfd4f919c81ba9e51de1b734679678a
Mert Koseoglu <bm.ksglu@gmail.com>
2023-04-21 13:11:38 +0300

    feat(session): add empty services and queries files

--- app/domains/Sessions/constants/endpoints.ts
+++ app/domains/Sessions/constants/endpoints.ts
@@ -1,26 +1,56 @@
-// enum SessionStatus {
-//   UPCOMING = 'upcoming',
-//   COMPLETED = 'completed',
-//   CANCELED = 'canceled',
-// }
-// TODO: I don't know it's right type or not
 import { SessionStatusType } from '@oxvo-mobile/constants/oxvo';
 
 const SESSION_ENDPOINTS = {
-  MY_SESSIONS: 'v1/sessions', // GET
-  CREATE_SESSION: 'v1/sessions', // POST
-  UPDATE_SESSION: 'v1/sessions', // PUT
+  // Endpoint created with a function
   UPDATE_SESSION_REPLY: 'v1/sessions', // PATCH
-  FILTER_SESSIONS: 'v1/sessions', // GET
+  UPDATE_SESSION: 'v1/sessions', // PUT
+  FILTERABLE_SESSIONS: 'v1/sessions', // GET
   SESSION_DETAIL: 'v1/sessions', // GET
+  // A constant endpoint
+  CREATE_SESSION: 'v1/sessions', // POST
+};
+
+enum FilteredSessionStatusType {
+  CANCELED = SessionStatusType.CANCELED,
+  COMPLETED = SessionStatusType.COMPLETED,
+  AWAITING = SessionStatusType.AWAITING,
+}
+
+type UpdateSessionReplyType = {
+  sessionId: string;
+  sessionReply: SessionStatusType;
 };
 
 const sessionEndpointFunctions = {
-  updateSession: (sessionId: string) => `${SESSION_ENDPOINTS.UPDATE_SESSION}/${sessionId}`,
-  filterSessions: (status: SessionStatusType) => {
-    const url = new URL(SESSION_ENDPOINTS.FILTER_SESSIONS);
-    const params = new URLSearchParams({ status });
+  updateSessionReply: ({ sessionId, sessionReply }: UpdateSessionReplyType) => {
+    /**
+     * @example
+     * PATCH /v1/sessions/:sessionId/update-reply?updateReply=sessionReply
+     */
+
+    const url = new URL(SESSION_ENDPOINTS.UPDATE_SESSION_REPLY);
+    url.pathname = `/${sessionId}`;
+    url.pathname = '/update-reply';
+
+    const params = new URLSearchParams({ updateReply: sessionReply });
     url.search = params.toString();
+
+    return url.toString();
+  },
+  updateSession: (sessionId: string) => `${SESSION_ENDPOINTS.UPDATE_SESSION}/${sessionId}`,
+  filterableSessions: (status?: FilteredSessionStatusType) => {
+    /**
+     * @example
+     * GET /v1/sessions
+     * GET /v1/sessions?status=FilteredSessionStatusType
+     */
+
+    const url = new URL(SESSION_ENDPOINTS.FILTERABLE_SESSIONS);
+
+    if (status) {
+      const params = new URLSearchParams({ status });
+      url.search = params.toString();
+    }
 
     return url.toString();
   },
