10fed1f91feda0699428b86bc6c7d4467369a8a7
Vehin Velat Eker <savataqe1@gmail.com>
2023-04-30 19:26:00 +0300

    fix:typo2

--- app/screens/PrivateScreens/Calendar/Calendar.tsx
+++ app/screens/PrivateScreens/Calendar/Calendar.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-import Settings from '@oxvo-mobile/components/Settings/Settings';
-
 const Calendar = () => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text>Calendar Screen</Text>
